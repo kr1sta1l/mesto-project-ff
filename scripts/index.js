3a571dc71860fd5ca8cde7a1b06f98e8090a4605
kr1sta1l <dmitriy_2003s@mail.ru>
2025-12-18 13:42:23 +0300

    Added manipulations with created cards
    Thats all?

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplateElement = document.querySelector('#card-template').content.querySelector('.card');
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+// Функция создания карточки
+function createCard(cardTitle, cardImage, onDeleteCard) {
+    const cardElement = cardTemplateElement.cloneNode(true);
+    cardElement.querySelector('.card__image').src = cardImage;
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__like-button').addEventListener('click', function (event) {
+        event.target.classList.toggle('card__like-button_is-active')
+    });
+    cardElement.querySelector('.card__delete-button').addEventListener('click', onDeleteCard);
+    return cardElement;
+  }
 
-// @todo: Вывести карточки на страницу
+
+// Функция удаления карточки
+function deleteCard(event) {
+    const card = event.target.closest('.card');
+    card.remove();
+}
+
+
+// Вывести карточки на страницу
+initialCards.forEach(card => {
+    placesList.append(createCard(card.name, card.link, deleteCard));
+});
